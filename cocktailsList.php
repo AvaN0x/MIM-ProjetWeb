cda0db16cade6d33654ea5a33da069c279d838bf
Patriot5701 <clement.lauer.57@gmail.com>
2021-10-13 17:36:28 +0200

    Update cocktailsList.php
    Affiche tous les cocktails avec leurs caractéristiques (sauf photos).

--- cocktailsList.php
+++ cocktailsList.php
@@ -2,33 +2,24 @@
     <h1>Liste des cocktails après navigation</h1>
     <!-- <h1>Liste des cocktails par recherche</h1> -->
     <div class="cocktails-list">
-        <article>
-            <div>
-                <i class="fas fa-heart"></i>
-                <i class="far fa-heart"></i>
-            </div>
-            <h2>Bloody qqch</h2>
-            <img src="res/Photos/Bloody_mary.jpg">
-            <ol>
-                <li>Vodka...</li>
-                <li>Vodka...</li>
-                <li>Vodka...</li>
-                <li>Vodka...</li>
-            </ol>
-        </article>
-        <article>
-            <div>
-                <i class="fas fa-heart"></i>
-                <i class="far fa-heart"></i>
-            </div>
-            <h2>Bloody qqch</h2>
-            <img src="res/Photos/Bloody_mary.jpg">
-            <ol>
-                <li>Vodka...</li>
-                <li>Vodka...</li>
-                <li>Vodka...</li>
-                <li>Vodka...</li>
-            </ol>
-        </article>
+        
+        <?php
+            foreach($Recettes as $key => $value){ 
+               echo "<article>
+                    <div>
+                        <i class=\"fas fa-heart\"></i>
+                        <i class=\"far fa-heart\"></i>
+                    </div>
+                    <h2>".$value["titre"]."</h2>
+                    <img src=\"res/Photos/Bloody_mary.jpg\">
+                    <ol> \n";
+               foreach ($value["index"] as $key2 => $value2) {
+                    echo "<li>".$value2."</li> \n";
+               }
+               echo "</ol>
+                    </article> \n";
+            }
+        ?>
+    
     </div>
 </section>
