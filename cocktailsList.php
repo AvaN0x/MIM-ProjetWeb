db1471a14b76513a299ad33f8d6a147e68a20ea2
AvaN0x <clement.ricatte57@gmail.com>
2021-10-16 17:01:45 +0200

    redid how coktails are displayed in cocktailsList

--- cocktailsList.php
+++ cocktailsList.php
@@ -1,56 +1,270 @@
+<?php
+// TODO @AvaN0x move this somewhere else
+
+// Source : https://github.com/lingtalfi/Bat/blob/779d4295814e68787d43c2d47e5455e634dccacb/StringTool.php#L466
+function removeAccents($str)
+{
+    static $map = [
+        // single letters
+        'à' => 'a',
+        'á' => 'a',
+        'â' => 'a',
+        'ã' => 'a',
+        'ä' => 'a',
+        'ą' => 'a',
+        'å' => 'a',
+        'ā' => 'a',
+        'ă' => 'a',
+        'ǎ' => 'a',
+        'ǻ' => 'a',
+        'À' => 'A',
+        'Á' => 'A',
+        'Â' => 'A',
+        'Ã' => 'A',
+        'Ä' => 'A',
+        'Ą' => 'A',
+        'Å' => 'A',
+        'Ā' => 'A',
+        'Ă' => 'A',
+        'Ǎ' => 'A',
+        'Ǻ' => 'A',
+
+        'ç' => 'c',
+        'ć' => 'c',
+        'ĉ' => 'c',
+        'ċ' => 'c',
+        'č' => 'c',
+        'Ç' => 'C',
+        'Ć' => 'C',
+        'Ĉ' => 'C',
+        'Ċ' => 'C',
+        'Č' => 'C',
+
+        'ď' => 'd',
+        'đ' => 'd',
+        'Ð' => 'D',
+        'Ď' => 'D',
+        'Đ' => 'D',
+
+        'è' => 'e',
+        'é' => 'e',
+        'ê' => 'e',
+        'ë' => 'e',
+        'ę' => 'e',
+        'ē' => 'e',
+        'ĕ' => 'e',
+        'ė' => 'e',
+        'ě' => 'e',
+        'È' => 'E',
+        'É' => 'E',
+        'Ê' => 'E',
+        'Ë' => 'E',
+        'Ę' => 'E',
+        'Ē' => 'E',
+        'Ĕ' => 'E',
+        'Ė' => 'E',
+        'Ě' => 'E',
+
+        'ƒ' => 'f',
+
+        'ĝ' => 'g',
+        'ğ' => 'g',
+        'ġ' => 'g',
+        'ģ' => 'g',
+        'Ĝ' => 'G',
+        'Ğ' => 'G',
+        'Ġ' => 'G',
+        'Ģ' => 'G',
+
+        'ĥ' => 'h',
+        'ħ' => 'h',
+        'Ĥ' => 'H',
+        'Ħ' => 'H',
+
+        'ì' => 'i',
+        'í' => 'i',
+        'î' => 'i',
+        'ï' => 'i',
+        'ĩ' => 'i',
+        'ī' => 'i',
+        'ĭ' => 'i',
+        'į' => 'i',
+        'ſ' => 'i',
+        'ǐ' => 'i',
+        'Ì' => 'I',
+        'Í' => 'I',
+        'Î' => 'I',
+        'Ï' => 'I',
+        'Ĩ' => 'I',
+        'Ī' => 'I',
+        'Ĭ' => 'I',
+        'Į' => 'I',
+        'İ' => 'I',
+        'Ǐ' => 'I',
+
+        'ĵ' => 'j',
+        'Ĵ' => 'J',
+
+        'ķ' => 'k',
+        'Ķ' => 'K',
+
+        'ł' => 'l',
+        'ĺ' => 'l',
+        'ļ' => 'l',
+        'ľ' => 'l',
+        'ŀ' => 'l',
+        'Ł' => 'L',
+        'Ĺ' => 'L',
+        'Ļ' => 'L',
+        'Ľ' => 'L',
+        'Ŀ' => 'L',
+
+        'ñ' => 'n',
+        'ń' => 'n',
+        'ņ' => 'n',
+        'ň' => 'n',
+        'ŉ' => 'n',
+        'Ñ' => 'N',
+        'Ń' => 'N',
+        'Ņ' => 'N',
+        'Ň' => 'N',
+
+        'ò' => 'o',
+        'ó' => 'o',
+        'ô' => 'o',
+        'õ' => 'o',
+        'ö' => 'o',
+        'ð' => 'o',
+        'ø' => 'o',
+        'ō' => 'o',
+        'ŏ' => 'o',
+        'ő' => 'o',
+        'ơ' => 'o',
+        'ǒ' => 'o',
+        'ǿ' => 'o',
+        'Ò' => 'O',
+        'Ó' => 'O',
+        'Ô' => 'O',
+        'Õ' => 'O',
+        'Ö' => 'O',
+        'Ø' => 'O',
+        'Ō' => 'O',
+        'Ŏ' => 'O',
+        'Ő' => 'O',
+        'Ơ' => 'O',
+        'Ǒ' => 'O',
+        'Ǿ' => 'O',
+
+        'ŕ' => 'r',
+        'ŗ' => 'r',
+        'ř' => 'r',
+        'Ŕ' => 'R',
+        'Ŗ' => 'R',
+        'Ř' => 'R',
+
+        'ś' => 's',
+        'š' => 's',
+        'ŝ' => 's',
+        'ş' => 's',
+        'Ś' => 'S',
+        'Š' => 'S',
+        'Ŝ' => 'S',
+        'Ş' => 'S',
+
+        'ţ' => 't',
+        'ť' => 't',
+        'ŧ' => 't',
+        'Ţ' => 'T',
+        'Ť' => 'T',
+        'Ŧ' => 'T',
+
+        'ù' => 'u',
+        'ú' => 'u',
+        'û' => 'u',
+        'ü' => 'u',
+        'ũ' => 'u',
+        'ū' => 'u',
+        'ŭ' => 'u',
+        'ů' => 'u',
+        'ű' => 'u',
+        'ų' => 'u',
+        'ư' => 'u',
+        'ǔ' => 'u',
+        'ǖ' => 'u',
+        'ǘ' => 'u',
+        'ǚ' => 'u',
+        'ǜ' => 'u',
+        'Ù' => 'U',
+        'Ú' => 'U',
+        'Û' => 'U',
+        'Ü' => 'U',
+        'Ũ' => 'U',
+        'Ū' => 'U',
+        'Ŭ' => 'U',
+        'Ů' => 'U',
+        'Ű' => 'U',
+        'Ų' => 'U',
+        'Ư' => 'U',
+        'Ǔ' => 'U',
+        'Ǖ' => 'U',
+        'Ǘ' => 'U',
+        'Ǚ' => 'U',
+        'Ǜ' => 'U',
+
+        'ŵ' => 'w',
+        'Ŵ' => 'W',
+
+        'ý' => 'y',
+        'ÿ' => 'y',
+        'ŷ' => 'y',
+        'Ý' => 'Y',
+        'Ÿ' => 'Y',
+        'Ŷ' => 'Y',
+
+        'ż' => 'z',
+        'ź' => 'z',
+        'ž' => 'z',
+        'Ż' => 'Z',
+        'Ź' => 'Z',
+        'Ž' => 'Z',
+
+        // accentuated ligatures
+        'Ǽ' => 'A',
+        'ǽ' => 'a',
+    ];
+    return strtr($str, $map);
+}
+
+?>
 <section>
     <h1>Liste des cocktails après navigation</h1>
     <!-- <h1>Liste des cocktails par recherche</h1> -->
     <div class="cocktails-list">
-
         <?php
-        foreach ($Recettes as $key => $value) {
-            $PhotoExist = false;
-            // TODO @AvaN0x optimisation
+        foreach ($Recettes as $recette) {
+            // Remove all accents from the title (titre)
+            // Then remove all spaces (` `) for underscores (`_`)
+            $imgFileName = str_replace(" ", "_", removeAccents($recette["titre"])) . '.jpg';
 
-            //On vérifie si une photo du cocktail existe
-            //On compare donc le titre du cocktail et le titre de la photo
-            //On les met au même format xxx_xxx
-            $temp = explode(" ", $value["titre"]);
-
-            //Les images du dossier ont un nom de maximum 2 mots, pas besoin de faire les autres
-            if (count($temp) == 2) {
-                $img = $temp[0] . "_" . strtolower($temp[1]);
-            } else {
-                $img = $value["titre"];
-            }
-
-            $Photos[] = scandir("res/Photos/");
-            $PhotosArray = $Photos[0];
-            array_splice($PhotosArray, 0, 2);
-
-            foreach ($PhotosArray as $key2 => $value2) {
-                $nomPhoto = explode(".", $value2);
-                if ($img == $nomPhoto[0]) {
-                    $PhotoExist = true;
-                }
-            }
-
-            //Si la photo n'existe pas, on affiche une image par défaut (Bloody Mary)
-            if ($PhotoExist == false) {
-                $img = "Bloody_mary";
-            }
-
-            echo "<article>
-                    <div>
-                        <i class=\"fas fa-heart\"></i>
-                        <i class=\"far fa-heart\"></i>
-                    </div>
-                    <h2>" . $value["titre"] . "</h2>
-                    <img src=\"res/Photos/$img.jpg\">
-                    <ul> \n";
-            foreach ($value["index"] as $key3 => $value3) {
-                echo "<li>" . $value3 . "</li> \n";
-            }
-            echo "</ul>
-                    </article> \n";
+            // If the file do not exist, then we set the default image
+            if (!file_exists("res/Photos/$imgFileName"))
+                $imgFileName = "cocktail.png";
+        ?>
+            <article>
+                <div>
+                    <i class="fas fa-heart"></i>
+                    <i class="far fa-heart"></i>
+                </div>
+                <h2><?= $recette["titre"] ?></h2>
+                <img src="res/Photos/<?= $imgFileName ?>">
+                <ul>
+                    <?php foreach ($recette["index"] as $value) : ?>
+                        <li><?= $value ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </article>
+        <?php
         }
         ?>
-
     </div>
 </section>
