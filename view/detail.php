248ec3f07654669162d1f6548036e73297898abc
Patriot5701 <clement.lauer.57@gmail.com>
2021-10-28 15:26:21 +0200

    Details view is done

--- view/detail.php
+++ view/detail.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 
 <?php
+require_once("model/utils.inc.php");
 include("includes/head.inc.php");
 ?>
 
@@ -13,12 +14,48 @@ include("includes/head.inc.php");
         <?php
         if (isset($recette)) :
             // TODO better display
+            $titre = $recette['titre'];
+            $ingredients = $recette['ingredients'];
+            $ingredients = explode('|', $ingredients);
+            $preparation = $recette['preparation'];
+
+            $img_path = str_replace(" ", "_", removeAccents($recette["titre"])) . '.jpg';
+            if (!file_exists("res/Photos/$img_path")){
+                $img_path = "cocktail.png";
+            }
         ?>
-            <pre>
-                <?php
-                print_r($recette);
-                ?>
-            </pre>
+            <article  class="detail">
+
+                <div class="fav">
+                <!-- TODO fav recette -->
+                    <a href="#">
+                    <!-- <i class="far fa-heart"></i> -->
+                    <i class="fas fa-heart"></i>
+                    </a>
+                </div>
+                
+                <h1><?= $titre ?></h1>
+                
+                <img class="img" src="res/Photos/<?= $img_path ?>" alt="<?= $img_path ?>" title="<?= $titre ?>"> 
+                
+                <div class="characteristics">
+                    <h3>Ingrédients</h3>
+                    <ul>
+                    <?php
+                    foreach ($ingredients as $value) {
+                        echo "<li>$value</li>\n";
+                    }
+                    ?>
+                    </ul>
+
+                    <h3>Préparation</h3>
+                    <p><?= $preparation ?></p>
+                 
+                </div>
+                
+                
+            </article>
+
         <?php
         else :
         ?>
