9662e5d63e21c90ed4a1e241569eaf14ecac7988
AvaN0x <clement.ricatte57@gmail.com>
2021-11-01 15:38:34 +0100

    few more css edits

--- view/includes/recetteCard.inc.php
+++ view/includes/recetteCard.inc.php
@@ -7,28 +7,26 @@ if (
     $recette = $Recettes[$recetteIndex];
 ?>
     <article class="recette-card" id="recette-card-<?= $recetteIndex ?>">
-        <!--
-        <?php
-        if (isset($recette["score"])) echo $recette["score"]; // TODO remove this (debug)
-        ?>
-        -->
-        <div class="recette-card-fav">
-            <!-- TODO fav recette -->
-            <a href="index.php">
-                <?php
-                if (isRecipeFavorite($recetteIndex)) {
-                    echo '<i class="fas fa-heart"></i>';
-                } else {
-                    echo '<i class="far fa-heart"></i>';
-                }
-                ?>
-            </a>
+        <div>
+            <div class="recette-card-fav">
+                <!-- TODO fav recette -->
+                <a href="index.php">
+                    <?php
+                    if (isRecipeFavorite($recetteIndex)) {
+                        echo '<i class="fas fa-heart"></i>';
+                    } else {
+                        echo '<i class="far fa-heart"></i>';
+                    }
+                    ?>
+                </a>
+            </div>
+            <h2>
+                <a href="index.php?route=detail&detail=<?= urlencode($recette["titre"]) ?>" target="_blank">
+                    <?= $recette["titre"] ?>
+                </a>
+            </h2>
         </div>
-        <h2>
-            <a href="index.php?route=detail&detail=<?= urlencode($recette["titre"]) ?>" target="_blank">
-                <?= $recette["titre"] ?>
-            </a>
-        </h2>
+
         <?php
         // Display the image if the file name exists
         if (isset($recette["img_file_name"])) {
