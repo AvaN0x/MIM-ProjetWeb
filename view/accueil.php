eaf892768e02e6adabcccf9d4c3a0b57dab3dec5
AvaN0x <clement.ricatte57@gmail.com>
2021-11-19 17:08:52 +0100

    feat: :lipstick: clean some code for accueil

--- view/accueil.php
+++ view/accueil.php
@@ -14,9 +14,31 @@ include(__DIR__ . "/includes/head.inc.php");
     ?>
 
     <div id="accueil">
-        <?php
-        include(__DIR__ . "/includes/navbar.inc.php");
-        ?>
+        <aside>
+            <h2>Aliment courant</h2>
+            <?php if ($ariane_has_error) : ?>
+                <p class="error"><i class="fas fa-exclamation-triangle"></i>Une erreur est survenue lors de la lecture du fil d'ariane demandé.</p>
+            <?php endif; ?>
+
+            <p class="aside-ariane">
+                <?php
+                foreach ($ariane as $key => $value) {
+                    echo '<span><a href="index.php?path=' . $value["path"] . '">' . $value["label"] . '</a></span>';
+                }
+                ?>
+            </p>
+
+            <h2>Sous-catégorie</h2>
+            <ul>
+                <?php
+                if (array_key_exists($actualAliment, $Hierarchie) && array_key_exists("sous-categorie", $Hierarchie[$actualAliment])) {
+                    foreach ($Hierarchie[$actualAliment]["sous-categorie"] as $aliment) {
+                        echo '<li><a href="index.php?route=' . LANDING_PAGE_NAME . '&path=' . $actualPath . '/' . urlencode($aliment) . '">' . $aliment . '</a></li>';
+                    }
+                }
+                ?>
+            </ul>
+        </aside>
         <main>
             <?php
             if ($searchType == SearchType::ARIANE) {
