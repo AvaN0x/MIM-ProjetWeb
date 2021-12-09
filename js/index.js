076c9da91476aa7ea0855db38ed714d8ef3439af
AvaN0x <clement.ricatte57@gmail.com>
2021-12-09 11:00:24 +0100

    add some comments for connection, and remove error tooltip if new result does not contain the error

--- js/index.js
+++ js/index.js
@@ -3,21 +3,42 @@ const connectUser = async () => {
     if (!form) return;
 
     const result = await (await fetch("index.php?route=ajax/connectuser", { method: "POST", body: new FormData(form) })).json();
+
+    // Player is logged
     if (result.success) {
         location.reload();
         return
     }
 
+    // Player is not logged, then we display errors
     if (result.data.errors) {
-        for (const error in result.data.errors) {
+        // For every errors fields
+        for (const error of ["login", "password"]) {
+            // If error exist in result
             const formElement = form.elements[error];
-            if (formElement)
-                formElement.classList.add("error-field");
-
             const errorElement = document.getElementById(`form-login-error-${error}`);
-            if (errorElement) {
-                errorElement.innerHTML = `<i class="fas fa-question"></i>`;
-                errorElement.dataset.errortooltip = result.data.errors[error];
+
+            if (result.data.errors[error]) {
+                // Add class error-field
+                if (formElement)
+                    formElement.classList.add("error-field");
+
+                // We set the content of the tooltip
+                if (errorElement) {
+                    errorElement.innerHTML = `<i class="fas fa-question"></i>`;
+                    errorElement.dataset.errortooltip = result.data.errors[error];
+                }
+            } else {
+                // Remove the error
+                // Remove class error-field
+                if (formElement)
+                    formElement.classList.remove("error-field");
+
+                // Remove the content from the tooltip
+                if (errorElement) {
+                    errorElement.innerHTML = "";
+                    errorElement.dataset.errortooltip = "";
+                }
             }
         }
     }
@@ -25,6 +46,8 @@ const connectUser = async () => {
 
 const disconnectUser = async () => {
     const result = await (await fetch("index.php?route=ajax/disconnectuser")).json();
+
+    // Player have disconnected, then success
     if (result.success)
         location.reload();
 }
